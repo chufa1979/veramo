d0481db9dccac68f88a765f7df2fb55cbe9bd7e2
Ruben Dario Vega <rvega@baufest.com>
2019-08-07 19:35:29 -0300

    Pagina Categoria

---
+++ tienda/js/categoria.js
@@ -0,0 +1,81 @@
+(function($){
+$(document).ready(function(){
+
+	////MENU////
+	var menuc = urlServicios + "nav.php";
+	var carga1 = $.get(menuc,  function(htmlexterno){ $("#navc").html(htmlexterno);});
+	////Footer///
+	var footerc = "footer.html";
+	var carga2 = $.get(footerc,  function(htmlexterno){ $("#footc").html(htmlexterno);});
+	  ////Contenido///
+	var vid = getParameterByName('vid');
+	var contenidoc = urlServicios + "categoria.php?vid="+vid;
+	
+	var carga3 = $.get(contenidoc,  function(htmlexterno){ $(".contenido").html(htmlexterno);});
+	////Cart///
+	var cartc = urlServicios + "cart.php";
+	var carga4 = $.get(cartc,  function(htmlexterno){ $("#cartc").html(htmlexterno);});
+
+	////Cart///
+	var encabc = urlServicios + "encabezado.php";
+	var carga5 = $.get(encabc,  function(htmlexterno){ $("#imagen_resp").html(htmlexterno);});
+
+
+	$.when( carga1, carga2, carga3, carga4, carga5 ).done(function(results1, results2, results3, results4, results5) {
+		$('.loadingpage').hide(); //oculto mediante id
+		$(".id_nombre_user").html($.session.get("nombreapellido"));
+		$(".id_nombre_user2").html("Bienvenido/a "+$.session.get("nombreapellido"));
+		////////
+
+		$("img.lazy").lazyload({
+			effect : "fadeIn"
+		});
+
+		var slideLeft = new Menu({
+			wrapper: '#o-wrapper',
+			type: 'slide-left',
+			menuOpenerClass: '.c-button',
+			maskId: '#c-mask'
+		});
+		
+		var slideLeftBtn = document.querySelector('#c-button--slide-left');
+		  
+		slideLeftBtn.addEventListener('click', function(e) {
+			e.preventDefault;
+			slideLeft.open();
+		});
+		
+		
+	}).fail(function() {
+		// will be called if one (or both) requests fail.  If a request does fail,
+		// the `done` callback will *not* be called even if one request succeeds.
+	});
+
+	///////////LLama a genera Meni
+	genera_menu();
+	  
+	
+
+		///////////////
+
+
+
+});
+})(jQuery);
+
+
+function MM_goToURL() { //v3.0
+	  var i, args=MM_goToURL.arguments; document.MM_returnValue = false;
+	  for (i=0; i<(args.length-1); i+=2) eval(args[i]+".location='"+args[i+1]+"'");
+   }
+
+   function getParameterByName(name) {
+    name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+    var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+    results = regex.exec(location.search);
+    return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+}
+
+function pasa(){
+	document.form_1.submit();
+}
--- tienda/js/home.js
+++ tienda/js/home.js
@@ -17,33 +17,19 @@ $(document).ready(function(){
 	$.when( carga1, carga2, carga3, carga4 ).done(function(results1, results2, results3, results4) {
 		$('.loadingpage').hide(); //oculto mediante id
 		$(".id_nombre_user").html($.session.get("nombreapellido"));
+		$(".id_nombre_user2").html("Bienvenido/a "+$.session.get("nombreapellido"));
+		
 		
 	}).fail(function() {
 		// will be called if one (or both) requests fail.  If a request does fail,
 		// the `done` callback will *not* be called even if one request succeeds.
 	});
 
-
+	///////////LLama a genera Meni
+	genera_menu();
 	  
 
-$('#cssmenu li.active').addClass('open').children('ul').show();
-	$('#cssmenu li.has-sub>a').on('click', function(){
-		$(this).removeAttr('href');
-		var element = $(this).parent('li');
-		if (element.hasClass('open')) {
-			element.removeClass('open');
-			element.find('li').removeClass('open');
-			element.find('ul').slideUp(200);
-		}
-		else {
-			element.addClass('open');
-			element.children('ul').slideDown(200);
-			element.siblings('li').children('ul').slideUp(200);
-			element.siblings('li').removeClass('open');
-			element.siblings('li').find('li').removeClass('open');
-			element.siblings('li').find('ul').slideUp(200);
-		}
-});
+
 
 });
 })(jQuery);
